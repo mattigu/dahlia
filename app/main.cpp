ae55725016766ac7bbdc9cb45c0c6005550a62b9
mattigu <gulinski.mateusz1@gmail.com>
2026-06-04 19:03:30 +0200

    feat: Add a minimal CLI

--- app/main.cpp
+++ app/main.cpp
@@ -1,1 +1,60 @@
-int main() { return 0; }
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <print>
+#include <sstream>
+
+#include "CLI/CLI.hpp"
+#include "dahlia_lib/Builtins.h"
+#include "dahlia_lib/Interpreter.h"
+#include "dahlia_lib/Lexer.h"
+#include "dahlia_lib/Parser.h"
+#include "dahlia_lib/Value.h"
+
+int main(int argc, char** argv) {
+    CLI::App app;
+
+    std::filesystem::path file_path;
+    std::string program_string;
+
+    auto* program_opt =
+        app.add_option("program", program_string, "The program to execute");
+    auto* file_opt =
+        app.add_option("-f,--file", file_path, "The file to execute")
+            ->transform(CLI::ExistingFile);
+
+    app.require_option(1);
+    CLI11_PARSE(app, argc, argv);
+
+    std::optional<std::ifstream> file_stream;
+    std::optional<std::istringstream> string_stream;
+
+    std::istream* input = nullptr;
+
+    if (*file_opt) {
+        file_stream.emplace(file_path);
+        input = &*file_stream;
+    } else if (*program_opt) {
+        string_stream.emplace(program_string);
+        input = &*string_stream;
+    }
+
+    auto parser = Parser(Lexer(*input, LexerOptions{}));
+
+    auto const program = parser.parse();
+    if (!program) {
+        std::println("Parsing failed");
+        return 1;
+    }
+
+    auto interpreter = Interpreter(makeDefaultBuiltins(std::cout),
+                                   InterpreterOpts{.max_call_depth = 200});
+
+    auto const result = interpreter.run(*program);
+    if (!result) {
+        std::println("Program failed");
+        return 1;
+    }
+    std::println("{}", *result);
+    return 0;
+}
