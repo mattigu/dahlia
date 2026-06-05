29311cfeb19d19000785d768ebe4565fa1f53e33
mattigu <gulinski.mateusz1@gmail.com>
2026-06-05 19:09:47 +0200

    feat: make cli program pass as string, explicit with a flag

--- app/main.cpp
+++ app/main.cpp
@@ -16,14 +16,19 @@ int main(int argc, char** argv) {
 
     std::string file_path;
     std::string program_string;
+    bool print_result{false};
 
-    auto* program_opt =
-        app.add_option("program", program_string, "The program to execute");
-    auto* file_opt =
-        app.add_option("-f,--file", file_path, "The file to execute")
+    auto* const input_group = app.add_option_group("input", "Input source");
+    auto* const program_opt = input_group->add_option(
+        "-p,--program", program_string, "The program to execute");
+    auto* const file_opt =
+        input_group->add_option("-f,--file", file_path, "The file to execute")
             ->transform(CLI::ExistingFile);
+    input_group->require_option(1);
 
-    app.require_option(1);
+    auto* const print_opt = app.add_flag(
+        "-r,--result", print_result, "Print the result of the main function");
+
     CLI11_PARSE(app, argc, argv);
 
     file_path = std::filesystem::path(file_path).lexically_normal().string();
@@ -63,6 +68,8 @@ int main(int argc, char** argv) {
                                          interpreter.stackTrace());
         return 1;
     }
-    std::println("{}", *result);
+    if (print_result) {
+        std::println("{}", *result);
+    }
     return 0;
 }
